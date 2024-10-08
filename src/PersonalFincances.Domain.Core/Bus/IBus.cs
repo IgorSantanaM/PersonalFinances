c15d84334a138b0517dbb42273e9acae0228f8ba
IgorSantanaM <igorsantanamedeiros17@gmail.com>
2024-10-08 16:36:10 -0400

    (feat): Added more design patterns in the domain layer

---
+++ src/PersonalFincances.Domain.Core/Bus/IBus.cs
@@ -0,0 +1,16 @@
+﻿using PersonalFincances.Domain.Core.Commands;
+using PersonalFincances.Domain.Core.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalFincances.Domain.Core.Bus
+{
+    public interface IBus
+    {
+        void SendCommand<T>(T theCommand) where T : Command;
+        void RaiseEvent<T>(T theEvent) where T : Event;
+    }
+}
