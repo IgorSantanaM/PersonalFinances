7aab51ccd5915cd2bf7e7c0bf43702c9773e4434
IgorSantanaM <igorsantanamedeiros17@gmail.com>
2024-10-10 16:54:54 -0400

    (fix): Fixed inmemory bux

--- src/PersonalFinances.Infra.CrossCutting/InMemoryBus.cs
+++ src/PersonalFinances.Infra.CrossCutting/InMemoryBus.cs
@@ -1,9 +1,33 @@
 ﻿using PersonalFincances.Domain.Core.Bus;
+using PersonalFincances.Domain.Core.Commands;
+using PersonalFincances.Domain.Core.Events;
+using PersonalFincances.Domain.Core.Notifications;
 
 namespace PersonalFinances.Infra.CrossCutting.Bus
 {
     public class InMemoryBus : IBus
     {
 
+        public static Func<IServiceProvider> ContainerAccessor { get; set; }
+        private static IServiceProvider Container => ContainerAccessor();
+        public void RaiseEvent<T>(T theEvent) where T : Event
+        {
+            Publish(theEvent);
+        }
+
+        public void SendCommand<T>(T theCommand) where T : Command
+        {
+            Publish(theCommand);
+        }
+
+        private static void Publish<T>(T message) where T : Message
+        {
+            if (Container == null) return;
+
+            var obj = Container.GetService(message.MessageType.Equals("DomainNotification")
+                ? typeof(IDomainNotificationHandler<T>)
+                : typeof(IHandler<T>)) as IHandler<T>;
+
+        }
     }
 }
